5e91ce3e302e1c017b699a4651ba168f63717cc7
MauricioVazz <mauricio.vaz@aluno.ifsp.edu.br>
2025-08-19 17:39:51 -0300

    criação da pasta routers

--- src/server.js
+++ src/server.js
@@ -1,40 +1,19 @@
 import express from 'express' // Importando o módulo express
+import profileRouter from './routes/profileRouter.js' // Importando o roteador de perfil
+import productRouter from './routes/productRouter.js' // Importando o roteador de produto
+import supplierRouter from './routes/supplierRouter.js' // Importando o roteador de fornecedor
+import carRouter from './routes/carRouter.js' // Importando o roteador de carro
 
 const app = express() // Criando uma instância do express
 const port = 3000 // Definindo a porta do servidor
 
-app.use(express.json()) // Middleware para analisar JSON
+app.use(express.json()) // Middleware para converter JSON em um objeto no req.body
 
-// app.put('/user', (req, res) => {
-//   //pegar dados do usuário
-//   res.json({message: 'Usuario atualizado com sucesso' })
-// })
-
-app.post('/profile', (req, res) => {
-  const dados = req.body
-  res.json({
-    message: 'Perfil criado com sucesso',
-    profile: dados
-  })
-})
-
-app.get('/profile', (req, res) => {
-  res.json({  message: 'Perfil retornado com sucesso' })
-})
-
-app.put('/profile', (req, res) => {
-  const dados = req.body
-  res.json({
-    message: 'Perfil atualizado com sucesso',
-    profile: dados
-  })
-})
-
-app.delete('/profile', (req, res) => {
-  res.json({ message: 'Perfil deletado com sucesso' })
-})
-
+app.use('/profile', profileRouter) // Usando o roteador de perfil para rotas que começam com /profile
+app.use('/product', productRouter) // Usando o roteador de produto para rotas que começam com /product
+app.use('/supplier', supplierRouter) // Usando o roteador de fornecedor para rotas que começam com /supplier
+app.use('/car', carRouter) // Usando o roteador de carro para rotas que começam com /car
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+    console.log(`Example app listening on port http://localhost:${port}`)
 })
