775cb1f51f3aff9a3d7555dabde67afe1db79549
MauricioVazz <mauricio.vaz@aluno.ifsp.edu.br>
2025-11-10 16:01:56 -0300

    cria o middleware de authenticator😎🥶

--- src/controllers/auth/loginController.js
+++ src/controllers/auth/loginController.js
@@ -3,37 +3,36 @@ import jwt from 'jsonwebtoken'
 import { getByEmail } from '../../models/profileModel.js'
 
 export const loginController = async (req, res) => {
-    // receber o email e a senha do corpo da requisição
+    // receber o email e a senha
     const { email, pass } = req.body
 
-    // comparar se o email e a senha estão corretos com o banco
+    // comparar se o email e a senha batem com o que está no banco de dados
     const user = await getByEmail(email)
     if (!user) {
         console.error('Usuário não encontrado para o email:', email)
-        return res.status(401).json({ message: 'Email ou Senha Inválida (email nao encontrado)' })
+        return res.status(401).json({ message: 'Email ou Senha Inválida' })
     }
 
     const passOk = await bcrypt.compare(pass, user.pass)
     if (!passOk) {
-        console.error('Senha inválida para o usuario:', email)
-        return res.status(401).json({ message: 'Email ou Senha Inválida (senha inválida)' })
+        console.error('Senha inválida')
+        return res.status(401).json({ message: 'Email ou Senha Inválida' })
     }
 
-    // se estiverem corretos, gerar um token (JWT)
-    const token = jwt.sign({id: user.id, email: user.email}, process.env.JWT_SECRET, {expiresIn: '1d'})
-
+    // se bater, gerar um token (JWT)
+    const token = await jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1d' })
     if (!token) {
-        return res.status(500).json({ message: 'Erro ao gerar o token de acesso' })
+        return res.status(500).json({ message: 'Erro ao gerar o Token de acesso!' })
     }
 
-    // enviar o token como resposta para o cliente
-        return res.status(200).json({
-            profile: {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                avatar: user.avatar,
-            },
-            token            
-        })
+    // enviar o token para o cliente
+    return res.status(200).json({
+        profile: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar
+        },
+        token
+    })
 }
---
+++ src/middleware/authenticator.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+
+export const authenticator = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Nâo autorizado' })
+    }
+
+    const token = authHeader.split(' ')[1] // Bearer <token>
+
+    if (!token) {
+        return res.status(401).json({ message: 'Nâo autorizado' })
+    }
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        req.userLogged = payload
+    } catch (err) {
+        console.error('Erro ao verificar o token:', err)
+        return res.status(401).json({ message: 'Token inválido' })
+    }        
+    next()
+}
